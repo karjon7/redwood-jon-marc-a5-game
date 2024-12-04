7bbca3c48b2b024c6ad284b1a8a644c46900aa76
Jon-Marc Redwood <karjon.redwood@gmail.com>
2024-12-04 12:24:33 -0500

    Add Player camera sway

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -1,17 +1,26 @@
 using Godot;
 using System;
+using System.Diagnostics;
 
 public partial class Player : CharacterBody2D
 {
+    [ExportGroup("Camera")]
+    [Export]
+    private int offsetAmplify = 20;
+
     [ExportGroup("Rotation")]
     [Export]
     private int playerRotationSpeed = 15;
 
-	[ExportGroup("Movement")]
+    [ExportGroup("Movement")]
     [Export]
-	private int playerSpeed = 10;
-	[Export]
-	private int playerAccel = 15;
+    private int playerSpeed = 10;
+    [Export]
+    private int playerAccel = 15;
+
+    [ExportGroup("Required Nodes")]
+    [Export]
+    public Camera2D Camera;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -21,6 +30,7 @@ public partial class Player : CharacterBody2D
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
     {
+        HandleCamera();
     }
 
     public override void _PhysicsProcess(double delta)
@@ -29,6 +39,19 @@ public partial class Player : CharacterBody2D
         HandleMovement(delta);
     }
 
+    private void HandleCamera()
+    {
+        int windowWidth = (int)ProjectSettings.GetSetting("display/window/size/viewport_width");
+        int windowHeight = (int)ProjectSettings.GetSetting("display/window/size/viewport_height");
+        Vector2 mousePos = GetGlobalMousePosition();
+
+        float horizontalOffset = (mousePos.X - GlobalPosition.X) / (windowWidth / 2);
+        float verticalOffset = (mousePos.Y - GlobalPosition.Y) / (windowHeight / 2);
+
+        Camera.Offset = new Vector2(horizontalOffset, verticalOffset) * offsetAmplify;
+        GD.Print(Camera.Offset);
+    }
+    
     private void HandleRotation(double delta)
     {
         Vector2 directionToTarget = GlobalPosition.DirectionTo(GetGlobalMousePosition());

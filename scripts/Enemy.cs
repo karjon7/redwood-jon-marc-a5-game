b53ae52683892c06749165b61b26b50fe8ce37a2
Jon-Marc Redwood <karjon.redwood@gmail.com>
2024-12-12 20:16:12 -0500

    Add Enemy movement

--- scripts/Enemy.cs
+++ scripts/Enemy.cs
@@ -3,13 +3,45 @@ using System;
 
 public partial class Enemy : CharacterBody2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    [Export]
+    private NavigationAgent2D navAgent;
+
+    [Export]
+    private int speed = 250;
+    [Export]
+    private int accel = 30;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        SetNavTargetPosition(Vector2.Zero);
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        if (!navAgent.IsTargetReached()) LookAt(Velocity);
+        HandleMovement(delta);
+    }
+
+    public void SetNavTargetPosition(Vector2 targetPos) 
+    {
+        navAgent.TargetPosition = targetPos;
+    }
+
+    private void HandleMovement(double delta)
+    {
+        Vector2 nextPosistion = navAgent.GetNextPathPosition();
+        Vector2 direction = GlobalPosition.DirectionTo(nextPosistion);
+        Vector2 wishVelocity = direction * speed;
+
+        wishVelocity = !navAgent.IsTargetReached() ? wishVelocity : Vector2.Zero;
+
+        float velocityX = Velocity.Lerp(wishVelocity, accel * (float)delta).X;
+        float velocityY = Velocity.Lerp(wishVelocity, accel * (float)delta).Y;
+
+        Velocity = new Vector2(velocityX, velocityY);
+        MoveAndSlide();
+    }
 }
